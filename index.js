32ca27eb0037046d468c9dbe0997d517e3d7d421
Alex Artemov <artemov.sandro@gmail.com>
2023-10-05 16:54:56 -0400

    practiced objects

---
+++ index.js
@@ -0,0 +1,20 @@
+const person = {
+    firstName: "Todd",
+    age: 28,
+}
+console.log(person.firstName)
+
+//add one to the person's age
+person.age++
+
+console.log(person.age)
+
+person.lastName = "Albert"
+/*
+const person = {
+    firstName: "Todd",
+    age: 29,
+    lastName: "Albert"
+}
+*/
+
